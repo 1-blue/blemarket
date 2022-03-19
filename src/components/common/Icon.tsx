ea31263e1ccc85c148288b47c26c874529e71869
1-blue <63289318+1-blue@users.noreply.github.com>
2022-03-19 18:18:16 +0900

    Feat: Add Icon component
    아이콘 컴포넌트 추가

---
+++ src/components/common/Icon.tsx
@@ -0,0 +1,137 @@
+import { useCallback } from "react";
+
+// type
+import { ICON_SHAPE } from "@src/types/index";
+
+interface IProps {
+  shape: ICON_SHAPE;
+  width?: number;
+  height?: number;
+}
+
+const Icon = ({ shape, width = 6, height = 6 }: IProps) => {
+  const getIcon = useCallback(() => {
+    switch (shape) {
+      case ICON_SHAPE.HOME:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          />
+        );
+      case ICON_SHAPE.NEWS:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+          />
+        );
+      case ICON_SHAPE.CHAT:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+          />
+        );
+      case ICON_SHAPE.CAMERA:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+          />
+        );
+      case ICON_SHAPE.USER:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+          />
+        );
+      case ICON_SHAPE.PLUS:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        );
+      case ICON_SHAPE.HEART:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        );
+      case ICON_SHAPE.CHECK:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        );
+      case ICON_SHAPE.PENCIL:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          />
+        );
+      case ICON_SHAPE.BACK:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+          />
+        );
+      case ICON_SHAPE.CART:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+          />
+        );
+      case ICON_SHAPE.BAG:
+        return (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+          />
+        );
+
+      default:
+        break;
+    }
+  }, [shape]);
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`w-${width} h-${height}`}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      {getIcon()}
+    </svg>
+  );
+};
+
+export default Icon;
