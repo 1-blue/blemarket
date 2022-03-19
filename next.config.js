746d34f58b8ec708c15e7917b9c64222fc248a0f
1-blue <63289318+1-blue@users.noreply.github.com>
2022-03-19 18:13:05 +0900

    Chore: Apply absolute path
    절대 경로 로직 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     reactRoot: true,
+  },
+  webpack(config) {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@src": path.resolve(__dirname, "src"),
+        },
+      },
+    };
   },
 };
 
