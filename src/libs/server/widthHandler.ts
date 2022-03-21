e2fc924db1fd3c98ede9b0df29c86714040ff9d5
1-blue <63289318+1-blue@users.noreply.github.com>
2022-03-21 17:30:37 +0900

    Feat: Add server handler function
    서버 라우팅을 도와주는 헬퍼함수 생성 ( H-O-F )

---
+++ src/libs/server/widthHandler.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type methodType = "GET" | "POST" | "DELETE";
+type fnType = (req: NextApiRequest, res: NextApiResponse) => Promise<any>;
+
+// 2022/03/21 - method에 따른 라우팅을 쉽게 처리해주는 HOF - by 1-blue
+const withHandler =
+  (method: methodType, fn: fnType) =>
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method !== method) return res.status(405).end();
+
+    try {
+      await fn(req, res);
+    } catch (error) {
+      return res.status(500).end();
+    }
+  };
+
+export default withHandler;
