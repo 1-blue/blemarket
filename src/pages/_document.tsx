8cdfec9c0e0414630d746ed8fb8ef0bcd7e8e082
1-blue <1-blue98@naver.com>
2022-04-07 22:24:32 +0900

    Feat: Add "_document"
    기본 템플릿만 추가

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class CustomDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="ko">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
