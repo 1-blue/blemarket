76ec24481ddeb302c788df8fb662aef04dfa3a7f
1-blue <1-blue98@naver.com>
2022-04-07 22:25:11 +0900

    Feat: Add "_middleware"
    쿠키의 존재 여부 확인해서 로그인 여부 판단

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,17 @@
+import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextResponse } from "next/server";
+
+export const middleware = (req: NextRequest, fe: NextFetchEvent) => {
+  // api 요청은 자체적으로 보호됨
+  if (req.url.includes("/api")) return;
+
+  // 로그인 페이지는 검사할 필요 없음
+  if (req.url.includes("/enter")) return;
+
+  // 쿠키의 존재 유무에 의해서만 로그인 여부를 판단 ( 유효성검사는 DB를 검색해봐야하므로 여기서 못하고 useUser에서 처리함 )
+  if (req.cookies["blemarket-session"]) return;
+
+  const url = req.nextUrl.clone();
+  url.pathname = "/enter";
+  return NextResponse.redirect(url);
+};
